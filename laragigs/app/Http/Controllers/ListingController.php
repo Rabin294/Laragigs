83e85abff8aeddd955cee026fdf5ea29689f99bc
Rabin294 <bloomworld294@yahoo.com>
2023-05-23 19:25:05 +0900

    adde crud operstion

--- laragigs/app/Http/Controllers/ListingController.php
+++ laragigs/app/Http/Controllers/ListingController.php
@@ -45,9 +45,51 @@ class ListingController extends Controller
             
     ]);
 
+    if($request->hasFile('logo')){
+        $formFields['logo']= $request->file('logo')->store('logos','public');
+    }
+
     Listing::create($formFields);
 
     return redirect('/')->with('message','Listing created successfully!');
 
     }
+
+     //Edit listing data
+     public function edit(Listing $listing){
+        return view ('listings.edit',['listing'=>$listing]);
+    }
+
+    //Update listing data
+    public function update(Request $request, Listing $listing){
+        $formFields =$request->validate([
+            'title'=>'required',
+            'company'=>['required'],
+            'location'=>'required',
+            'website'=>'required',
+            'tags'=>'required',
+            'email'=>['required','email'],
+            'description'=>'required'
+            
+    ]);
+
+    if($request->hasFile('logo')){
+        $formFields['logo']= $request->file('logo')->update('logos','public');
+    }
+
+    $listing->update($formFields);
+
+    return back()->with('message','Listing updated successfully!');
+
+    }
+
+    //Delete listing
+    public function destroy(Listing $listing){
+        $listing->delete();
+        return redirect('/')->with('message','Listing deleted successfully!');
+
+    }
+
 }
+
+
--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -40,6 +40,14 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 //Store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+//Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
